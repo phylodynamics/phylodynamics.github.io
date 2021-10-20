e083e950358262b7c7aa6176616acd58f1f14121
phylodynamics <37640371+phylodynamics@users.noreply.github.com>
2021-10-20 10:32:34 -0400

    Add files via upload

---
+++ files/RunAfterIntallingBEAGLEv4.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir "$HOME/Library/Java/Extensions/"
+echo "Directory successfully created"
+ln -s "/usr/local/lib/libhmsbeagle-jni.jnilib" "$HOME/Library/Java/Extensions/libhmsbeagle-jni.jnilib"
+echo "Symlink successfully created"
